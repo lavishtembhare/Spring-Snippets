c4d90e413554723628bf904a4b82a955b46acdba
lavishtembhare <lavishtembhare93@gmail.com>
2025-11-17 01:34:41 +0530

    update

---
+++ Demo-Project/src/main/java/com/example/demo/Main.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@SpringBootApplication
+public class Main {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+		BeanFactory fact=new ClassPathXmlBeanFactory(resource);
+		student s=fact.getBean("student");
+		
+		
+	}
+
+}
