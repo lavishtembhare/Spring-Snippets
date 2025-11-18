00d1aae08930eaeb2f7328e46691f4bfb38a8c64
lavishtembhare <lavishtembhare93@gmail.com>
2025-11-18 16:17:29 +0530

    Dependency Injection

---
+++ Dependency-Injection/src/main/java/com/lavish/first/DependencyInjectionApplication.java
@@ -0,0 +1,13 @@
+package com.lavish.first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependencyInjectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DependencyInjectionApplication.class, args);
+	}
+
+}
---
+++ Dependency-Injection/src/test/java/com/lavish/first/DependencyInjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lavish.first;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyInjectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
