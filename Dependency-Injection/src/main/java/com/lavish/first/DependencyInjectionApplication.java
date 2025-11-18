c65f5716e8606b867ed19a0995c6456a13869b59
lavishtembhare <lavishtembhare93@gmail.com>
2025-11-18 23:12:35 +0530

    Core Done

--- Dependency-Injection/src/main/java/com/lavish/first/DependencyInjectionApplication.java
+++ Dependency-Injection/src/main/java/com/lavish/first/DependencyInjectionApplication.java
@@ -2,12 +2,16 @@ package com.lavish.first;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DependencyInjectionApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DependencyInjectionApplication.class, args);
+		ApplicationContext context = SpringApplication.run(DependencyInjectionApplication.class, args);
+		Student s= context.getBean(Student.class);
+		s.code();
+		
 	}
 
 }
--- Dependency-Injection/src/main/java/com/lavish/first/Student.java
+++ Dependency-Injection/src/main/java/com/lavish/first/Student.java
@@ -1,5 +1,8 @@
 package com.lavish.first;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Student {
 public void code() {
 	System.out.println("Coding.....");
