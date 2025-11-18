fb79758ed273e5587884afc4d363e230cefb53be
lavishtembhare <lavishtembhare93@gmail.com>
2025-11-18 23:56:36 +0530

    AutoWire

---
+++ Dependency-Injection/src/main/java/com/lavish/first/Laptop.java
@@ -0,0 +1,13 @@
+package com.lavish.first;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+
+	public void compile() {
+		// TODO Auto-generated method stub
+		System.out.println("Compiling");
+	}
+
+}
--- Dependency-Injection/src/main/java/com/lavish/first/Student.java
+++ Dependency-Injection/src/main/java/com/lavish/first/Student.java
@@ -1,10 +1,14 @@
 package com.lavish.first;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Student {
+	@Autowired
+	Laptop lap;
+	
 public void code() {
-	System.out.println("Coding.....");
+	lap.compile();
 }
 }
