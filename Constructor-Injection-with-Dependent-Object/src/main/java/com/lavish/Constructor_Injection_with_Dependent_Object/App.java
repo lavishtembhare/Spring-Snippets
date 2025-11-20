adb9d6330d1505e82cfc390315f4a24b3d792e48
lavishtembhare <lavishtembhare93@gmail.com>
2025-11-20 14:51:59 +0530

    Constructor Injection with Dependent Object

---
+++ Constructor-Injection-with-Dependent-Object/src/main/java/com/lavish/Constructor_Injection_with_Dependent_Object/App.java
@@ -0,0 +1,13 @@
+package com.lavish.Constructor_Injection_with_Dependent_Object;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Constructor-Injection-with-Dependent-Object/src/test/java/com/lavish/Constructor_Injection_with_Dependent_Object/AppTest.java
@@ -0,0 +1,38 @@
+package com.lavish.Constructor_Injection_with_Dependent_Object;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
