8f8b9fb7a6bb5ad9d5632e106b210830d420e70c
lavishtembhare <lavishtembhare93@gmail.com>
2025-11-21 14:51:02 +0530

    Update

--- Dependency-Injection-with-Factory-Method/src/main/java/com/lavish/App.java
+++ Dependency-Injection-with-Factory-Method/src/main/java/com/lavish/App.java
@@ -1,13 +1,12 @@
 package com.lavish;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.springframework.context.ApplicationContext;  
+import org.springframework.context.support.ClassPathXmlApplicationContext;  
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        A a = (A) context.getBean("a");
+        a.msg();
     }
 }
