3cb3db684fee48bd599a6140e62bc44202dc9dcc
lavishtembhare <lavishtembhare93@gmail.com>
2025-11-21 15:54:02 +0530

    Jdbc Template

---
+++ JDBC/JdbcTemplate/src/main/java/com/lavish/JdbcTemplate/App.java
@@ -0,0 +1,13 @@
+package com.lavish.JdbcTemplate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
