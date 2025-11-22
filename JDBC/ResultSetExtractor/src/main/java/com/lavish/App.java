9c2b97c3b23c87b583a1281182692196261776b9
lavishtembhare <lavishtembhare93@gmail.com>
2025-11-23 02:05:38 +0530

    Update

--- JDBC/ResultSetExtractor/src/main/java/com/lavish/App.java
+++ JDBC/ResultSetExtractor/src/main/java/com/lavish/App.java
@@ -1,13 +1,21 @@
 package com.lavish;
 
-/**
- * Hello world!
- *
- */
+import java.util.List;   // <-- IMPORTANT
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class App 
 {
-    public static void main( String[] args )
+    public static void main(String[] args)
     {
-        System.out.println( "Hello World!" );
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");  
+
+        EmployeeDao dao = (EmployeeDao) ctx.getBean("edao");  
+
+        List<Employee> list = dao.getAllEmployees();  
+
+        for (Employee e : list) { 
+            System.out.println(e);
+        }
     }
 }
