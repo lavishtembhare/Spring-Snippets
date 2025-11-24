2d09076ba1d93bfc44ce751a1503ad152b8c9007
lavishtembhare <lavishtembhare93@gmail.com>
2025-11-24 14:41:43 +0530

    Update

--- Spring with ORM Frameworks/Hibernate-and-Spring-Integration/src/main/java/com/lavish/App.java
+++ Spring with ORM Frameworks/Hibernate-and-Spring-Integration/src/main/java/com/lavish/App.java
@@ -1,13 +1,22 @@
 package com.lavish;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        EmployeeDao dao = context.getBean("employeeDao", EmployeeDao.class);
+
+        Employee e = new Employee();
+        e.setName("Varun");
+        e.setSalary(50000);
+
+        dao.saveEmployee(e);
+
+        System.out.println("Employee inserted successfully!");
     }
 }
